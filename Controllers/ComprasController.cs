7867a0f44ed63d4740211de2eabf32929f31ef2a
Davi Paes <davipaes102@gmail.com>
2023-12-19 19:26:41 -0300

    Mudanças de estilização, mudanças na model, novos controllers adicionados.

---
+++ Controllers/ComprasController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProjetoFinal.Models;
+
+namespace ProjetoFinal.Controllers
+{
+    public class ComprasController : Controller
+    {
+        private readonly Contexto _context;
+
+        public ComprasController(Contexto context)
+        {
+            _context = context;
+        }
+
+        // GET: Compras
+        public async Task<IActionResult> Index()
+        {
+            var contexto = _context.Compras.Include(c => c.Cliente);
+            return View(await contexto.ToListAsync());
+        }
+
+        // GET: Compras/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Compras == null)
+            {
+                return NotFound();
+            }
+
+            var compra = await _context.Compras
+                .Include(c => c.Cliente)
+                .FirstOrDefaultAsync(m => m.CompraID == id);
+            if (compra == null)
+            {
+                return NotFound();
+            }
+
+            return View(compra);
+        }
+
+        // GET: Compras/Create
+        public IActionResult Create()
+        {
+            ViewData["ClienteID"] = new SelectList(_context.Clientes, "ClienteID", "CPF");
+            return View();
+        }
+
+        // POST: Compras/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CompraID,ClienteID,Data,FormaPagamento")] Compra compra)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(compra);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClienteID"] = new SelectList(_context.Clientes, "ClienteID", "CPF", compra.ClienteID);
+            return View(compra);
+        }
+
+        // GET: Compras/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Compras == null)
+            {
+                return NotFound();
+            }
+
+            var compra = await _context.Compras.FindAsync(id);
+            if (compra == null)
+            {
+                return NotFound();
+            }
+            ViewData["ClienteID"] = new SelectList(_context.Clientes, "ClienteID", "CPF", compra.ClienteID);
+            return View(compra);
+        }
+
+        // POST: Compras/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CompraID,ClienteID,Data,FormaPagamento")] Compra compra)
+        {
+            if (id != compra.CompraID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(compra);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CompraExists(compra.CompraID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClienteID"] = new SelectList(_context.Clientes, "ClienteID", "CPF", compra.ClienteID);
+            return View(compra);
+        }
+
+        // GET: Compras/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Compras == null)
+            {
+                return NotFound();
+            }
+
+            var compra = await _context.Compras
+                .Include(c => c.Cliente)
+                .FirstOrDefaultAsync(m => m.CompraID == id);
+            if (compra == null)
+            {
+                return NotFound();
+            }
+
+            return View(compra);
+        }
+
+        // POST: Compras/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Compras == null)
+            {
+                return Problem("Entity set 'Contexto.Compras'  is null.");
+            }
+            var compra = await _context.Compras.FindAsync(id);
+            if (compra != null)
+            {
+                _context.Compras.Remove(compra);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CompraExists(int id)
+        {
+          return (_context.Compras?.Any(e => e.CompraID == id)).GetValueOrDefault();
+        }
+    }
+}
