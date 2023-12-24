d287cd131a97ebe0672958ee4cf3158260849158
Davi Paes <davipaes102@gmail.com>
2023-12-24 16:13:44 -0300

    Mudanças na view, model e novas migrations.

--- Models/FechamentoCaixa.cs
+++ Models/FechamentoCaixa.cs
@@ -3,6 +3,8 @@
     public class FechamentoCaixa
     {
         public int FechamentoID { get; set; }
-
+        public DateTime Data { get; set; }
+        public int CompraID { get; set; }
+        virtual public Compra Compra { get; set; }
     }
 }
